b4c45bc4f4c24026b154fa7816f2b1c150f8e1cd
Usagi Ito <usagi@WonderRabbitProject.net>
2013-08-01 14:23:46 +0900

    add main.html

--- main.ts
+++ main.ts
@@ -3,7 +3,14 @@
 class tsg_1_t extends xcanvas.game_t {
 }
 
-var tst_1 = () => {
-  var c = <HTMLCanvasElement>document.getElementById('draw_target');
-  return new tsg_1_t(c.getContext('2d')).run();
-}
+var tsg_1 = null;
+
+window.addEventListener('load', ()=>{
+  var c = new HTMLCanvasElement();
+  c.setAttribute('id', 'draw_target');
+  c.setAttribute('width', '720');
+  c.setAttribute('height', '480');
+  document.appendChild(c);
+  var x = c.getContext('2d');
+  tsg_1 = new tsg_1_t(x).run();
+});
