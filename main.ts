500911916c98ee5ad1f6d32781cc46be885df453
Usagi Ito <usagi@WonderRabbitProject.net>
2013-08-01 16:11:24 +0900

    update submodule xcanvas

--- main.ts
+++ main.ts
@@ -6,11 +6,11 @@ class tsg_1_t extends xcanvas.game_t {
 var tsg_1 = null;
 
 window.addEventListener('load', ()=>{
-  var c = new HTMLCanvasElement();
+  var c = document.createElement('canvas');
   c.setAttribute('id', 'draw_target');
   c.setAttribute('width', '720');
   c.setAttribute('height', '480');
-  document.appendChild(c);
+  document.body.appendChild(c);
   var x = c.getContext('2d');
   tsg_1 = new tsg_1_t(x).run();
 });
