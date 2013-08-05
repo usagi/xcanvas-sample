75a6a94c9e839db5694f1f8467bf64d0a8f444cd
Usagi Ito <usagi@WonderRabbitProject.net>
2013-08-05 10:42:01 +0900

    :)

--- main.ts
+++ main.ts
@@ -1,6 +1,26 @@
 /// <reference path="xcanvas/xcanvas.ts"/>
 
 class tsg_1_t extends xcanvas.game_t {
+  sm: xcanvas.scene_manager_t;
+  im: xcanvas.input_manager_t;
+  
+  initialize(){
+    super.initialize();
+    this.sm = new xcanvas.input_manager_t();
+    this.components.push(this.sm);
+    this.im = new xcanvas.input_manager_t();
+    this.components.push(this.im);
+  }
+}
+
+class scene_1_t extends xcanvas.scene_t {
+  get tsg_1() { return <tsg_1>this.game; }
+
+  
+  update(game_time: xcanvas.game_time_t){
+    super.update(game_time);
+    console.log(this.tsg_1.im);
+  }
 }
 
 var tsg_1 = null;
@@ -14,3 +34,4 @@ window.addEventListener('load', ()=>{
   var x = c.getContext('2d');
   tsg_1 = new tsg_1_t(x).run();
 });
+
